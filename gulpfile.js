6254f7d4c6e3f0287817460dd3c284658368e26b
Michael Murphy <murphym18@gmail.com>
2015-06-30 21:34:26 -0700

    Progress on sass

--- gulpfile.js
+++ gulpfile.js
@@ -8,30 +8,52 @@ var mainBowerFiles = require('main-bower-files');
 var shell = require('gulp-shell');
 var exec = require('child_process').exec;
 var path = require('path');
+var sass = require('gulp-ruby-sass') ;
 
 gulp.task('default', ['build', 'copy-libs'])
 
 gulp.task('build', function() {
    return gulp.src("app/**/*.js")
-      .pipe(preprocess({context: config.defines}))
+      .pipe(preprocess({
+         context: config.defines
+      }))
       .pipe(babel(config.babel))
       .pipe(gulp.dest("dist"));
 });
 
-gulp.task('copy-libs', function() {
-    return gulp.src(mainBowerFiles())
-        .pipe(gulp.dest('dist'))
+gulp.task('css', function() { 
+   return gulp.src(config.sassPath + '/style.scss') .pipe(sass({ 
+         style: 'compressed',
+ loadPath: [ config.sassPath, config.bowerDir +
+            '/bootstrap-sass-official/assets/stylesheets', 
+            config.bowerDir + '/fontawesome/scss' 
+         ] 
+      }) 
+      .on("error", notify.onError(function(error) { 
+         return "Error: " + error.message; 
+      })))  .pipe(gulp.dest('./dist/css')); 
 });
 
-gulp.task('r.js',['build', 'copy-libs'], function(cb) {
+gulp.task('icons', function() { 
+   return gulp.src(config.bowerDir + '/fontawesome/fonts/**.*') 
+      .pipe(gulp.dest('./public/fonts')); 
+});
+
+gulp.task('copy-libs', function() {
+   return gulp.src(mainBowerFiles())
+      .pipe(gulp.dest('dist'))
+});
+
+gulp.task('r.js', ['build', 'copy-libs'], function(cb) {
    var rjs = path.normalize('./node_modules/requirejs/bin/r.js');
-   exec('node ' + rjs + ' -o requirejs.build.js', function (err, stdout, stderr) {
-     console.log(stdout);
-     console.log(stderr);
-     cb(err);
+   exec('node ' + rjs + ' -o requirejs.build.js', function(err, stdout,
+      stderr) {
+      console.log(stdout);
+      console.log(stderr);
+      cb(err);
    });
 });
 
 gulp.task('clean', function(cb) {
-    del(['dist'], cb)
+   del(['dist'], cb)
 });

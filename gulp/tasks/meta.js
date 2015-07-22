52f3673b28b7b654e60da50705e87e28b2ac312b
Michael Murphy <murphym18@gmail.com>
2015-07-22 14:09:39 -0700

    Fix code style

--- gulp/tasks/meta.js
+++ gulp/tasks/meta.js
@@ -9,8 +9,6 @@ var rename = require('gulp-rename');
 var handlebars = require('gulp-compile-handlebars');
 var metaDir = path.resolve(config['pub-directory'], 'meta');
 
-
-
 gulp.task('meta', ['meta-favicons', 'meta-humans', 'meta-robots',
    'meta-crossdomain'
 ]);
@@ -29,23 +27,18 @@ gulp.task('meta-favicons', function(cb) {
    var colors = ["blue", "darkblue", "gray", "green", "orange"];
    var i = Math.floor(Math.random() * colors.length);
    var color = colors[i];
-
-   var files = path.resolve(config['pub-directory'], 'icons', color) +
-      '/*';
-   return gulp.src(files)
+   var files = path.resolve(config['pub-directory'], 'icons', color);
+   
+   return gulp.src(files + '/*')
       .pipe(gulp.dest(config['out-directory']));
 });
-
-
-gulp.task('meta-humans', humans);
 
 var monthNames = ["January", "February", "March", "April", "May", "June",
    "July", "August", "September", "October", "November", "December"
 ];
 
-function humans() {
+gulp.task('meta-humans', function() {
    var now = new Date();
-
    var locals = {
       year: now.getFullYear(),
       month: monthNames[now.getMonth()],
@@ -56,4 +49,4 @@ function humans() {
       .pipe(handlebars(locals))
       .pipe(rename('humans.txt'))
       .pipe(gulp.dest(config['out-directory']));
-}
+});
